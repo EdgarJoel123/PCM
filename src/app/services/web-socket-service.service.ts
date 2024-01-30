b20a24836d6c8da02ca46230a571301a3cfd91c2
Edgar Sanchez <esanchez1169joel@gmail.com>
2024-01-30 14:39:48 -0500

    Elimnacion de configracion de sockets

--- src/app/services/web-socket-service.service.ts
+++ src/app/services/web-socket-service.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class WebSocketService {
 
+  /*constructor(private socket: Socket){
 
+  }
+
+  subcribeToUpdate(){
+    return this.socket.fromEvent<any>('actualizacionEjecucionProyectos');
+  }
+
+
+  senMessage(msg: string){
+    this.socket.emit('mesasge', msg);
+  }
+
+  getMessage(){
+    return this.socket.fromEvent<any>('message');
+  } */
 }
